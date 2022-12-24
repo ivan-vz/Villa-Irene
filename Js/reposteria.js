e40a95c82ce9206d3775237849c5830e6af7660a
Ivan Vecino <ivanveci28@gmail.com>
2022-12-24 19:26:50 -0300

    remodelacion_2

--- Js/reposteria.js
+++ Js/reposteria.js
@@ -8,35 +8,74 @@ document.addEventListener('DOMContentLoaded', () => {
 
 let mostrarProductos = () => {
     let cuerpo = document.getElementById("variosDulces");
-    console.log(productos);
     productos['varios-dulces'].forEach((dulce) => {
-        cuerpo.innerHTML += `
-        <div class="card col my-3" style="width: 18rem; cursor: pointer;" data-toggle="modal" data-target="#${dulce.nombre}" onclick="mostrarModal('${dulce.nombre}')">
+
+        let cont = 0;
+        let parteInicial= "";
+        let fotosCarousel = "";
+        let parteFinal = "";
+
+        parteInicial= `
+        <div class="card col my-3" style="width: 18rem; cursor: pointer;" data-bs-toggle="modal"
+            onclick="mostrarModal('${dulce.nombre}1')">
             <img class="card-img-top mt-2" src="${dulce.img[0]}" alt="Card image cap">
             <div class="card-body text-center">
-                <h5 class="card-title">${dulce.nombre}</h5>
+            <h5 class="card-title">${dulce.nombre}</h5>
             </div>
         </div>
-
-        <div class="modal fade" id="${dulce.nombre}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
+    
+        <div class="modal fade" id="${dulce.nombre}1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
                 <div class="modal-content">
+            
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">${dulce.nombre}</h5>
+                        <div id="${dulce.nombre}2" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                
+        `
+
+        fotosCarousel += `
+        <div class="carousel-item active">
+            <img src="${dulce.img[0]}" style="width: 30em; height: 20em;" class="d-block" alt="${dulce.nombre}">
+        </div>
+        `
+        for(let i = 1; i < dulce.img.length; i++){
+            fotosCarousel += `
+            <div class="carousel-item">
+                <img src="${dulce.img[i]}" style="width: 30em; height: 20em;" class="d-block" alt="${dulce.nombre}">
+            </div>
+            `
+        }
+        parteFinal = `
                     </div>
-                    <div class="modal-body">
-                        <div></div>
-                        <div>
-                            ${dulce.desripcion}
-                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#${dulce.nombre}2" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#${dulce.nombre}2" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
                     </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+
+                <div class="modal-body">
+                    <div>
+                        <h5 class="modal-title">${dulce.nombre}</h5>
                     </div>
+                    <div>
+                        ${dulce.desripcion}
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                 </div>
             </div>
         </div>
+    </div>
         `
+
+        cuerpo.innerHTML += parteInicial + fotosCarousel + parteFinal;
     })
 }
 
